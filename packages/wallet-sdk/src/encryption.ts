e740ac4f610f392c2e34c21d29d8f5732a0b9119
Hank Stoever <hstove@gmail.com>
2021-01-10 23:55:19 -0800

    fix: naming

--- packages/wallet-sdk/src/encryption.ts
+++ packages/wallet-sdk/src/encryption.ts
@@ -1,4 +1,17 @@
-import { encryptMnemonic } from '@stacks/encryption';
+import { encryptMnemonic, decryptMnemonic } from '@stacks/encryption';
+import { decrypt as triplesecDecrypt } from 'triplesec';
+
+/**
+ * Decrypt an encrypted mnemonic phrase with a password.
+ * Legacy triplesec encrypted payloads are also supported.
+ * @param data - Buffer or hex-encoded string of the encrypted mnemonic
+ * @param password - Password for data
+ * @return the raw mnemonic phrase
+ */
+export async function decrypt(dataBuffer: Buffer | string, password: string): Promise<string> {
+  const result = await decryptMnemonic(dataBuffer, password, triplesecDecrypt);
+  return result;
+}
 
 /**
  * Encrypt a raw mnemonic phrase to be password protected
